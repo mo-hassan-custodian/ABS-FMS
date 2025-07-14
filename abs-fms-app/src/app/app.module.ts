9f43b7480eff11a6f494223d02c46148ef548f51
mo-hassan-custodian <mhassan@custodianinsurance.com>
2025-07-14 04:05:59 +0100

    Upgrade to Angular 18.2.13 - Core packages updated successfully

--- abs-fms-app/src/app/app.module.ts
+++ abs-fms-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,37 +16,31 @@ import { requisitionReducer } from './app-state/reducers/requisition.reducer';
 import { ApiInterceptor } from './interceptors/api.interceptor';
 import { ApiTestComponent } from './components/api-test/api-test.component';
 
-@NgModule({
-  declarations: [AppComponent, ApiTestComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    ToastrModule.forRoot({
-      timeOut: 3000,
-      positionClass: 'toast-top-right',
-      preventDuplicates: true,
-      progressBar: true,
-      closeButton: true,
-      enableHtml: true,
-    }),
-    AppRoutingModule,
-    MaterialModule,
-    DashboardModule,
-    LayoutModule,
-    StoreModule.forRoot({ requisitionState: requisitionReducer }, {}),
-    EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-  ],
-  providers: [
-    { provide: MAT_DIALOG_DATA, useValue: {} },
-    { provide: MatDialogRef, useValue: {} },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ApiInterceptor,
-      multi: true,
-    },
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, ApiTestComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot({
+            timeOut: 3000,
+            positionClass: 'toast-top-right',
+            preventDuplicates: true,
+            progressBar: true,
+            closeButton: true,
+            enableHtml: true,
+        }),
+        AppRoutingModule,
+        MaterialModule,
+        DashboardModule,
+        LayoutModule,
+        StoreModule.forRoot({ requisitionState: requisitionReducer }, {}),
+        EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })], providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ApiInterceptor,
+            multi: true,
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
